a360ce05c16727c5ffcf64ff9dfd73a06447618c
eibrahimarisoy <eibrahimarisoy@gmail.com>
2020-05-30 21:36:36 +0300

    updated Link and ClickCount models.

--- link/models.py
+++ link/models.py
@@ -6,10 +6,17 @@ BASE_URL = 'http://localhost:8000/'
 
 
 class Link(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
     exact_link = models.URLField(verbose_name='Asıl Link')
-    hide_link = models.URLField(verbose_name='Gizli/Yönlendirilmiş Link')
-
+    hide_link = models.URLField(
+        verbose_name='Gizli/Yönlendirilmiş Link',
+        unique=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,15 +24,28 @@ class Link(models.Model):
         verbose_name = 'Link'
         verbose_name_plural = 'Linkler'
         ordering = ['created_at']
+    
+    def __str__(self):
+        return f"{self.exact_link} - {self.hide_link}"
 
 
 class ClickCount(models.Model):
-    link = models.ForeignKey('Link', on_delete=models.SET_NULL, blank=True, null=True)
+    link = models.ForeignKey(
+        'Link',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        unique_for_date="date"
+    )
     date = models.DateField(verbose_name='Tarih')
-    count = models.IntegerField(default=0, verbose_name='Tıklanma Sayısı')
+    count = models.IntegerField(
+        default=0,
+        verbose_name='Tıklanma Sayısı')
 
     class Meta:
         verbose_name = 'Link Tıklanma Sayısı'
         verbose_name_plural = 'Linklerin Günlük Tıklanma Sayısı'
         ordering = ['link']
 
+    def __str__(self):
+        return f"{self.link} - {self.date} - {self.count}"
